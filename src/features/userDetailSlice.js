60bb773b264378f8c7f5ad84ed2a7c9c0dd1f805
umar hussain <90171344+umar2001016@users.noreply.github.com>
2023-07-03 05:09:56 -0700

    crud-with search

---
+++ src/features/userDetailSlice.js
@@ -0,0 +1,155 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const createUser = createAsyncThunk(
+  "createUser",
+  async (data, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(
+        "https://641dd63d945125fff3d75742.mockapi.io/crud",
+        data,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
+
+export const showUser = createAsyncThunk(
+  "showUser",
+  async (args, { rejectWithValue, dispatch }) => {
+    try {
+      const response = await axios.get(
+        "https://641dd63d945125fff3d75742.mockapi.io/crud"
+      );
+      const result = response.data;
+      console.log(result);
+
+      
+      if (result.length) {
+        return result;
+      } else {
+        dispatch(searchUserError("Record Not Found"));
+      }
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
+
+export const deleteUser = createAsyncThunk(
+  "deleteUser",
+  async (id, { rejectWithValue }) => {
+    try {
+      const response = await axios.delete(
+        `https://641dd63d945125fff3d75742.mockapi.io/crud/${id}`
+      );
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
+
+export const updateUser = createAsyncThunk(
+  "updateUser",
+  async (data, { rejectWithValue }) => {
+    try {
+      const response = await axios.put(
+        `https://641dd63d945125fff3d75742.mockapi.io/crud/${data.id}`,
+        data,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
+
+export const userDetail = createSlice({
+  name: "userDetail",
+  initialState: {
+    users: [],
+    loading: false,
+    error: null,
+    searchData: [],
+  },
+  reducers: {
+    searchUser: (state, action) => {
+      state.searchData = action.payload;
+      state.error = null;
+    },
+    searchUserError: (state, action) => {
+      state.error = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createUser.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(createUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.users.push(action.payload);
+      })
+      .addCase(createUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload.message;
+      })
+      .addCase(showUser.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(showUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.users = action.payload;
+      })
+      .addCase(showUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(deleteUser.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(deleteUser.fulfilled, (state, action) => {
+        state.loading = false;
+        const { id } = action.payload;
+        if (id) {
+          state.users = state.users.filter((ele) => ele.id !== id);
+        }
+      })
+      .addCase(deleteUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(updateUser.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(updateUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.users = state.users.map((ele) =>
+          ele.id === action.payload.id ? action.payload : ele
+        );
+      })
+      .addCase(updateUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload.message;
+      });
+  },
+});
+
+export const { searchUser, searchUserError } = userDetail.actions;
+
+export const getRecordNotFoundErrorMessage = () => "Record Not Found";
+
+export default userDetail.reducer;
